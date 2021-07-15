a066fca569eb21a5efaad8267999a87bf8e7a621
isuru <isuru@hatch.nz>
2021-07-15 21:05:00 +1200

    bug fixed v1.2

--- index.js
+++ index.js
@@ -12,52 +12,53 @@ startDownload();
 async function startDownload () {
     await downloadTorrent();
 
-    for (let i = 2; i < allPages; i++) {
-        await axios.get(`https://yts.mx/browse-movies/0/all/all/6/rating/2015-2021/en?page=${i}`).then(async (response) => {
-            const html = HTMLParser.parse(response.data);
-            let movies = html.querySelectorAll(".browse-movie-link");
-            if (movies && movies.length > 0) {
-                console.log(`Found ${movies.length} movies!`);
-                let counter = 1;
-                for (const movie of movies) {
-                    console.log(`Running ${counter++}/${movies.length}`);
-                    if (movie.attributes && movie.attributes.href) {
-                        axios.get(movie.attributes.href).then((pageResponse) => {
-                            const singlePageHtml = HTMLParser.parse(pageResponse.data);
-                            let torrentFileLinks = singlePageHtml.querySelectorAll('#movie-info p.hidden-xs.hidden-sm a');
-                            if (torrentFileLinks && torrentFileLinks.length > 0) {
-                                for (const toDownloadTorrentFileNode of torrentFileLinks) {
-                                    if (toDownloadTorrentFileNode.text.substr(0, 4) === '1080') {
-                                        const toDownloadTorrentFileLink = toDownloadTorrentFileNode && toDownloadTorrentFileNode.attributes && toDownloadTorrentFileNode.attributes.href ? toDownloadTorrentFileNode.attributes.href : null;
-                                        let db = fileManager.readFile(dbFilePath);
-                                        if (db !== null && db !== false && toDownloadTorrentFileLink) {
-                                            db = JSON.parse(db);
-                                            db.push({
-                                                link: toDownloadTorrentFileLink,
-                                                status: 1, // 1 = ready to download, 2 - downloading, 3 - downloaded.
-                                            });
-                                            fileManager.writeFile(dbFilePath, JSON.stringify(db));
-                                            console.log('URL has added to the DB!\n');
-                                        }
-                                    }
-                                }
-                            }
-                        });
-                    }
-                    await sleep(5000);
-                }
-            }
-            console.log('\n\nDownload complete!');
-            console.log('starting torrent download...');
-            await downloadTorrent();
-        });
-    }
+    // for (let i = 2; i < allPages; i++) {
+    //     await axios.get(`https://yts.mx/browse-movies/0/all/all/6/rating/2015-2021/en?page=${i}`).then(async (response) => {
+    //         const html = HTMLParser.parse(response.data);
+    //         let movies = html.querySelectorAll(".browse-movie-link");
+    //         if (movies && movies.length > 0) {
+    //             console.log(`Found ${movies.length} movies!`);
+    //             let counter = 1;
+    //             for (const movie of movies) {
+    //                 console.log(`Running ${counter++}/${movies.length}`);
+    //                 if (movie.attributes && movie.attributes.href) {
+    //                     axios.get(movie.attributes.href).then((pageResponse) => {
+    //                         const singlePageHtml = HTMLParser.parse(pageResponse.data);
+    //                         let torrentFileLinks = singlePageHtml.querySelectorAll('#movie-info p.hidden-xs.hidden-sm a');
+    //                         if (torrentFileLinks && torrentFileLinks.length > 0) {
+    //                             for (const toDownloadTorrentFileNode of torrentFileLinks) {
+    //                                 if (toDownloadTorrentFileNode.text.substr(0, 4) === '1080') {
+    //                                     const toDownloadTorrentFileLink = toDownloadTorrentFileNode && toDownloadTorrentFileNode.attributes && toDownloadTorrentFileNode.attributes.href ? toDownloadTorrentFileNode.attributes.href : null;
+    //                                     let db = fileManager.readFile(dbFilePath);
+    //                                     if (db !== null && db !== false && toDownloadTorrentFileLink) {
+    //                                         db = JSON.parse(db);
+    //                                         db.push({
+    //                                             link: toDownloadTorrentFileLink,
+    //                                             status: 1, // 1 = ready to download, 2 - downloading, 3 - downloaded.
+    //                                         });
+    //                                         fileManager.writeFile(dbFilePath, JSON.stringify(db));
+    //                                         console.log('URL has added to the DB!\n');
+    //                                     }
+    //                                 }
+    //                             }
+    //                         }
+    //                     });
+    //                 }
+    //                 await sleep(5000);
+    //             }
+    //         }
+    //         console.log('\n\nDownload complete!');
+    //         console.log('starting torrent download...');
+    //         await downloadTorrent();
+    //     });
+    // }
 }
 
 /**
  * Download torrent using the torrent link
  */
 async function downloadTorrent () {
+    process.chdir('E:/plex-library/movies');
     let db = fileManager.readFile(dbFilePath);
     if (db !== null && db !== false) {
         const torrentLinks = JSON.parse(db);
